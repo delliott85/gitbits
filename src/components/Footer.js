5db3d596106463297e9aa376df5f31ed2e53d14f
Dean Elliott <dean@deanelliott.me>
2021-03-05 19:21:21 +0000

    Fix security warning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import styles from './Footer.module.scss';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <p>Site by <a href="https://deanelliott.me" target="_blank">Dean Elliott</a>. &copy; {new Date().getFullYear()}</p>
+            <p>Site by <a href="https://deanelliott.me" target="_blank" rel="nofollow noreferrer">Dean Elliott</a>. &copy; {new Date().getFullYear()}</p>
         </footer>
     )
 };
